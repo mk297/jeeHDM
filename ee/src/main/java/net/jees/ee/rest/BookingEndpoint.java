c1f5606fe6feb84e8c8dbf1d178597c487fe6339
M <M@MICHABOOK.fritz.box>
2017-04-22 19:58:02 +0200

    Removed bookSeat/deleteSeat from BookingEndpoint. Added Exercice Comments

--- ee/src/main/java/net/jees/ee/rest/BookingEndpoint.java
+++ ee/src/main/java/net/jees/ee/rest/BookingEndpoint.java
@@ -11,37 +11,48 @@ import javax.ws.rs.core.MediaType;
 import net.jees.ee.json.JsonHelper;
 import net.jees.ee.service.BookingService;
 
-@Path(value = "/booking")
+@Path("/booking")
 public class BookingEndpoint {
 	@Inject
 	private BookingService bookingService;
 
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public String bookSeat(@FormParam(value = "seat") int seat, @FormParam(value = "row") int row,
-	    @FormParam(value = "personID") int personID) {
-		return JsonHelper.jsonfySuccessAnswer(bookingService.bookMovie(seat, row, personID));
-	}
+	
+	//-- Aufgabe 1 --- 
+	//Um neue Kinobesucher anlegen zu können, soll die "POST"-Methode createPerson() realisiert werden. 
+	//Diese soll auf den relativen Pfad /booking/createPerson gemapped werden.
+	//Als Parameter soll der Vorname "preName" und Nachname "surName" als Form-Parameter übergeben werden. 
+	//Als Response soll eine Erfolgsmeldung der gerade angelegten Person an den Client zurückgegeben werden. 
+	//Tipp: Verwende die Funktion createPerson(...) der bereitstehenden Klasse BookingService und die Funktion jsonfySuccessAnswer(...) der Klasse JsonHelper aus dem Package json.  
+	//----------------
+	
+	
+	//Code here
+	
+	//-- Aufgabe 2 --- 
+	//Um einen Platz zu reservieren, soll die "POST"-Methode bookSeat() realisiert werden. 
+	//Diese soll auf den root gemapped werden.
+	//Als Parameter soll die Sitznummer "seat" und "row" (Integer) als Form-Parameter übergeben werden.
+	//Des Weiteren soll eine "personID" (Integer) benötigt, damit Tickets einer Person zugeordnet werden kann.
+	//Als Response soll eine Erfolgsmeldung des gerade reservierten Tickets an den Client zurückgegeben werden.  
+	//----------------
+	
+	//Code here
 
-	@POST
-	@Path(value = "/createPerson")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String createPerson(@FormParam(value = "preName") String preName,
-	    @FormParam(value = "surName") String surName) {
-		return JsonHelper.jsonfySuccessAnswer(bookingService.createPerson(preName, surName));
-	}
-
-	@POST
-	@Path(value = "/deletePerson")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String deletePerson(@FormParam(value = "personID") int personID) {
-		return JsonHelper.jsonfySuccessAnswer(bookingService.deletePerson(personID));
-	}
-
+	
+	
+	//-----------------------------
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getReservations() {
 		System.out.println("get");
 		return JsonHelper.jsonfyReservations(bookingService.getReservations());
 	}
+	
+	@POST
+	@Path(value = "/deletePerson")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String deletePerson(@FormParam(value = "personID") int personID) {
+		return JsonHelper.jsonfySuccessAnswer(bookingService.deletePerson(personID));
+	}
+	
 }
